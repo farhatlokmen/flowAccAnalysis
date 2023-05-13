91f680466e2ece20ad79d60ed611f07cce28cc38
Farhat Lokmen <34503787+farhatlokmen@users.noreply.github.com>
2023-05-13 18:40:34 +0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Green Areas Doha",
+    page_icon="🌿",
+)
+
+st.write("# Welcome to GADoha Platform! 👋")
+
+#st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    GADoha is an open-source web application built using modern tools to assist both 
+    the municipality and private sector engineers in better managing green areas in Doha.
+
+    **👈 Select a demo from the sidebar** to see some examples of what GADoha can help with!
+    
+    #### Want to learn more about the developer?
+    Check out [Lokmen Farhat Portfolio](https://farhatlokmen.github.io/website/)
+    
+"""
+)
