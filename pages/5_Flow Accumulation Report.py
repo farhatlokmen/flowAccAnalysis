6e3112006ec65d2a75440c4f140686c800fe5b76
Farhat Lokmen <34503787+farhatlokmen@users.noreply.github.com>
2023-05-21 07:36:23 +0300

    Update 5_Flow Accumulation Report.py
    Added option to specify autoMode

--- pages/5_Flow Accumulation Report.py
+++ pages/5_Flow Accumulation Report.py
@@ -2,8 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import numpy as np
+import pdfkit
 import jinja2 
 import os
+import tools
 from urllib.error import URLError
 
 st.set_page_config(page_title="Flow Accumulation Analysis", page_icon="🌊")
@@ -22,7 +25,6 @@ try:
     else:
         pwd_container.empty()
         st.success("You have successfully logged in !")
-
         uploaded_files = st.file_uploader("", accept_multiple_files=True) 
         if not(uploaded_files is not None and len(uploaded_files)==2):
             st.warning("Upload only the two files corresponding to designed and observed flow.")
@@ -33,6 +35,21 @@ try:
             idx_designed = 1 - idx_observed
             designedFlow_DF = pd.read_csv(uploaded_files[idx_designed])
             observedFlow_DF = pd.read_csv(uploaded_files[idx_observed], header=None)            
+            autoMode = st.radio('Automatic Irrigation Mode',['Sequence','Parallel'])
+            valvesID = designedFlow_DF["valve"]
+            if autoMode == "Parallel":
+                valvesID = np.unique(designedFlow_DF["Group"]) 
+                valveNames = []
+                GPM = []
+                lat = []
+                lon = []
+                for valveID in valvesID:
+                    valveNames.append(list(designedFlow_DF[designedFlow_DF["Group"]==valveID]["valve"])[0]) 
+                    GPM.append(np.sum(designedFlow_DF[designedFlow_DF["Group"]==valveID]["GPM"]))
+                    lat.append(list(designedFlow_DF[designedFlow_DF["Group"]==valveID]["lat"])[0])
+                    lon.append(list(designedFlow_DF[designedFlow_DF["Group"]==valveID]["lon"])[0])
+                designedFlow_DF = pd.DataFrame({'valve': valveNames,'GPM': GPM,'lat':lat,'lon':lon})                
+            valvesID = designedFlow_DF["valve"] 
             version = st.radio('ICC System',['ICC','ICCpro'])   
             flowsensor_systemError = st.radio('Exclude days when flow sensor was not active',['yes','no'])          
             if version == 'ICCpro':
@@ -42,10 +59,7 @@ try:
                 observedFlow_DF.drop(index=[1], inplace=True)
                 observedFlow_DF.reset_index(drop=True, inplace=True)
                 observedFlow_DF.columns = range(observedFlow_DF.columns.size)
-                valveNames_all = []
-                for i in range(0,len(observedFlow_DF.iloc[:, 0]),2):
-                    valveNames_all.append(observedFlow_DF.iloc[i, 0])        
-                valves = st.multiselect("Choose Irrigation Valve(s)", list(np.unique(valveNames_all)), [])                
+                valves = st.multiselect("Choose Irrigation Valve(s)", list(valvesID), [])                 
                 if not valves:
                     st.warning("Please select at least one irrigation valve.")
                 else:                    
@@ -63,10 +77,10 @@ try:
                             obsTF = float(observedFlow_DF.iat[index_observed[0], 2])
                             hours = int(observedFlow_DF.iloc[index_observed[0]][3][:2])
                             minutes = int(observedFlow_DF.iloc[index_observed[0]][3][3:5])                            
-                            if ((hours*60+minutes) != 0) & (flowsensor_systemError == "no"): # Exclude out days with irrigation time being 0                                
+                            if ((hours*60+minutes) != 0) & (flowsensor_systemError == "no"):                                
                                 time = hours*60+minutes
                                 Time.append(time)
-                                desTF = time*float(designedFlow_DF.iat[index_designed[0], 1])   
+                                desTF = time*float(designedFlow_DF.iat[index_designed[0], 1])  
                                 valveNames.append(valve)
                                 dates.append(day)
                                 totalFlow_designed.append(desTF) 
@@ -87,20 +101,20 @@ try:
                                         'Time':Time,
                                         'Flow_observed':totalFlow_observed,
                                         'Flow_designed':totalFlow_designed,
-                                        'errorP':errorP})    
+                                        'errorP':errorP})  
                     if len(df1)==0:
                         st.warning(" Flow sensor was not active during the specified period!")
-                    elif len(df1)>=1: # Only do subsequent analysis, when flow sensor has been active for at least 1 day                    
+                    elif len(df1)>=1: 
                         startDate = pd.to_datetime(st.date_input('Start Date', df1[df1['valves'] == valves[0]]['date'].iloc[0]))
                         endDate = pd.to_datetime(st.date_input('End Date', df1[df1['valves'] == valves[0]]['date'].iloc[-1]))
-                        df1_filtered1 = df1[(df1['date'] >= startDate) & (df1['date'] < endDate)] # Filter DataFrame based on used-defined dates                        
+                        df1_filtered1 = df1[(df1['date'] >= startDate) & (df1['date'] < endDate)] 
                         avgErrorP = []
                         n_obs = []            
                         for valve in valves:  
                             avgErrorP.append(np.round(np.mean(df1_filtered1[df1_filtered1['valves'] == valve]['errorP']),2))
                             n_obs.append(len(df1_filtered1[(df1_filtered1['valves'] == valve)]['errorP']))                        
                         df2 = pd.DataFrame({'valve': valves,'avgErrorP':avgErrorP,'nb days':n_obs}) 
-                        st.write(df2)                         
+                        st.write(df2) 
                         st.write('Average error of all selected valves (%):', np.round(np.mean(avgErrorP), 2))
                         chart_lines = (
                         alt.Chart(df1_filtered1, title="")
@@ -114,21 +128,44 @@ try:
                         st.altair_chart(chart_lines, use_container_width=True)
                         selected_indices = [np.where(designedFlow_DF["valve"] == valve)[0][0] for valve in valves]                     
                         st.map(designedFlow_DF[['lat','lon']].iloc[selected_indices], zoom=12)
+                        
+                        # export options
                         pages_path = os.path.dirname(__file__)
-                        app_path = os.path.dirname(pages_path)                        
+                        app_path = os.path.dirname(pages_path)
+                        
                         path=os.path.join(app_path,'./templates')
                         templateLoader = jinja2.FileSystemLoader(searchpath=path)
                         templateEnv = jinja2.Environment(loader=templateLoader)
                         TEMPLATE_FILE = "template.html"
                         template = templateEnv.get_template( TEMPLATE_FILE )
+
                         export_as_pdf = st.button("Export Report")
+
                         if export_as_pdf:
-                            st.success("Report successfully downloaded!")    
+                            outputText = template.render(df=df2, timePeriod= 'from '+str(startDate)[:10]+' to '+str(endDate)[:10])
+                            file_name = os.path.join(app_path,'reports', "report.html")
+                            html_file = open(file_name, 'w', encoding='utf-8')
+                            html_file.write(outputText)
+                            html_file.close()
+
+                            file_name = os.path.join(app_path,'reports', "report.html")
+                            if os.path.isfile(file_name):         
+                                #read html code to string
+                                html_file = open(file_name, 'r', encoding='utf-8')
+                                source_code = html_file.read()                                 
+                                # write the page break
+                                source_code += '<div class="new-page"></div>'
+                            else:
+                                st.warning(f'{file_name} not found')
+
+                            path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+                            config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+                            PDF_TARGET_FILE = os.path.join(app_path,'reports', "report.pdf")
+                            pdfkit.from_string(source_code, PDF_TARGET_FILE, configuration=config) 
+                            html_file.close()
+                            st.success("Report successfully downloaded!") 
             elif version == 'ICC':
-                valveNames_all = []
-                for i in range(0,len(designedFlow_DF.iloc[:, 0]),2):
-                    valveNames_all.append(designedFlow_DF.iloc[i, 0])        
-                valves = st.multiselect("Choose Irrigation Valve(s)", list(np.unique(valveNames_all)), [])                
+                valves = st.multiselect("Choose Irrigation Valve(s)", list(valvesID), [])                
                 if not valves:
                     st.warning("Please select at least one irrigation valve.")
                 else:
@@ -144,16 +181,15 @@ try:
                             day = observedFlow_DF[observedFlow_DF[5]==valve].iat[k,14]
                             index_observed = observedFlow_DF[(observedFlow_DF[14]==day) & (observedFlow_DF[5]==valve)][18].index
                             index_designed = designedFlow_DF[designedFlow_DF["valve"]==valve]["GPM"].index                            
-                            index_observed_flow = index_observed[0]
+                            index_observed_flow = index_observed[0] 
                             index_observed_time = index_observed[1] 
                             hours = int(observedFlow_DF.iat[index_observed_time, 18][11:13])
                             minutes = int(observedFlow_DF.iat[index_observed_time, 18][14:16])
                             obsTF = 1000*float(observedFlow_DF.iat[index_observed_flow, 18])
-                            if ((hours*60+minutes) != 0) & (flowsensor_systemError == "no"): # Exclude out days with irrigation time being 0                                
+                            if ((hours*60+minutes) != 0) & (flowsensor_systemError == "no"):                                 
                                 time = hours*60+minutes
                                 Time.append(time)
-                                desTF = time*float(designedFlow_DF.iat[index_designed[0], 1])   
-                                
+                                desTF = time*float(designedFlow_DF.iat[index_designed[0], 1]) 
                                 valveNames.append(valve)
                                 dates.append(day)
                                 totalFlow_designed.append(desTF) 
@@ -163,7 +199,6 @@ try:
                                 time = hours*60+minutes
                                 Time.append(time)
                                 desTF = time*float(designedFlow_DF.iat[index_designed[0], 1]) 
-
                                 valveNames.append(valve)
                                 dates.append(day)
                                 totalFlow_designed.append(desTF) 
@@ -201,16 +236,44 @@ try:
                         st.altair_chart(chart_lines, use_container_width=True)
                         selected_indices = [np.where(designedFlow_DF["valve"] == valve)[0][0] for valve in valves]                     
                         st.map(designedFlow_DF[['lat','lon']].iloc[selected_indices], zoom=12)
+                        
+                        # export options
                         pages_path = os.path.dirname(__file__)
-                        app_path = os.path.dirname(pages_path)                        
+                        app_path = os.path.dirname(pages_path)
+                        
                         path=os.path.join(app_path,'./templates')
                         templateLoader = jinja2.FileSystemLoader(searchpath=path)
                         templateEnv = jinja2.Environment(loader=templateLoader)
                         TEMPLATE_FILE = "template.html"
                         template = templateEnv.get_template( TEMPLATE_FILE )
+
                         export_as_pdf = st.button("Export Report")
+
                         if export_as_pdf:
-                            st.success("Report successfully downloaded!") 
+                            outputText = template.render(df=df2, timePeriod= 'from '+str(startDate)[:10]+' to '+str(endDate)[:10])
+                            file_name = os.path.join(app_path,'reports', "report.html")
+                            html_file = open(file_name, 'w', encoding='utf-8')
+                            html_file.write(outputText)
+                            html_file.close()
+
+                            file_name = os.path.join(app_path,'reports', "report.html")
+                            if os.path.isfile(file_name):         
+                                #read html code to string
+                                html_file = open(file_name, 'r', encoding='utf-8')
+                                source_code = html_file.read() 
+                                
+                                # write the page break
+                                source_code += '<div class="new-page"></div>'
+                            else:
+                                st.warning(f'{file_name} not found')
+
+                            path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+                            config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+                            PDF_TARGET_FILE = os.path.join(app_path,'reports', "report.pdf")
+                            pdfkit.from_string(source_code, PDF_TARGET_FILE, configuration=config) 
+                            html_file.close()
+                            st.success("Report successfully downloaded!")                    
+
 except URLError as e:
     st.error(
         """
